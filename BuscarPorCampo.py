cdbecb48133b9bfffa87c248c102985b61222548
ROBERSON FARID AMAGUA LEMA <roberson.amagua@orenesgrupo.com>
2024-04-22 21:38:59 +0200

    Vector Files

--- BuscarPorCampo.py
+++ BuscarPorCampo.py
@@ -1,35 +1,29 @@
 from lxml import etree
+import os
 
-# DEF TAG <JugadorId>"Value"</JugadorId>
-def extract_jugador_ids(xml_file, output_file):
-    jugador_ids = {}
-    total_jugador_ids = 0
-    context = etree.iterparse(xml_file, events=("end",))
+def extract_jugador_ids(xml_files):
+    for xml_file in xml_files:
+        jugador_ids = {}
+        total_jugador_ids = 0
+        output_file = os.path.splitext(xml_file)[0] + "_jugador_ids.txt"
 
-    with open(output_file, "w") as out_file:
-        for event, elem in context:
-            if elem.tag.endswith("JugadorId"):
-                jugador_id = elem.text.strip() if elem.text else None
-                print(f"Elemento: {elem.tag}, Texto: {jugador_id}")
-                jugador_ids[elem.tag] = jugador_id
-                total_jugador_ids += 1
-                out_file.write(f"{jugador_id}\n")
+        with open(output_file, "w") as out_file:
+            context = etree.iterparse(xml_file, events=("end",))
 
-            elem.clear()  # clear buffer
+            for event, elem in context:
+                if elem.tag.endswith("JugadorId"):
+                    jugador_id = elem.text.strip() if elem.text else None
+                    jugador_ids.setdefault(elem.tag, []).append(jugador_id)
+                    total_jugador_ids += 1
+                    out_file.write(f"{jugador_id}\n")
 
-    return jugador_ids, total_jugador_ids
+                elem.clear() #limpiar buffer cada vez que se lee un archivo
+
+        print(f"Total de JugadorId encontrados en {xml_file}: {total_jugador_ids}")
 
 def main():
-    
-    # Next Changes the path
-    xml_file = "test.xml"
-    
-    output_file = "jugador_ids.txt"
-
-    jugador_ids, total_jugador_ids = extract_jugador_ids(xml_file, output_file)
-
-    print(jugador_ids)
-    print("Total de JugadorId encontrados:", total_jugador_ids)
+    xml_files = ["enveloped1.xml", "enveloped2.xml"]  # Nombre de los XML
+    extract_jugador_ids(xml_files)
 
 if __name__ == "__main__":
     main()
